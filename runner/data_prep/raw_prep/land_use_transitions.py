ac3705ca5f300ecdb2c7a6c6078fe423cdefe7ee
iglesias-caio-bcg <iglesias.caio@bcg.com>
2022-11-22 13:25:14 -0300

    get new land_use_transitions and save old one as a backup

--- runner/data_prep/raw_prep/land_use_transitions.py
+++ runner/data_prep/raw_prep/land_use_transitions.py
@@ -6,37 +6,80 @@ from ..data_utils import pipe
 from runner import io
 
 
+@pipe
 def _select_cols_and_treat_df(df_land_use_transitions):
     """
     Select cols and treat land use transtition DataFrame.
     """
 
     df_land_use_transitions["location"] = (
-        df_land_use_transitions["city"] + " (" + df_land_use_transitions["state"] + ")"
+        df_land_use_transitions["municipality"]
+        + " ("
+        + df_land_use_transitions["UF"]
+        + ")"
     )
     dict_rename_transition = {
-        str(i) + "-" + str(i + 1): pd.to_datetime(str(i)) for i in range(1985, 2020)
+        str(i) + "-" + str(i + 1): pd.to_datetime(str(i)) for i in range(1985, 2021)
     }
     relevant_cols = [
-        "city",
-        "state",
+        "municipality",
+        "UF",
         "location",
-        "from_level_0",
-        "from_level_1",
-        "from_level_2",
-        "from_level_3",
-        "from_level_4",
-        "to_level_0",
-        "to_level_1",
-        "to_level_2",
-        "to_level_3",
-        "to_level_4",
+        "biome",
+        "level_0",
+        "level_1",
+        "level_2",
+        "level_3",
+        "level_4",
+        "from_class",
+        "to_class",
     ]
     df_land_use_transitions = df_land_use_transitions[
         relevant_cols + list(dict_rename_transition.keys())
     ]
 
     return df_land_use_transitions
+
+
+@pipe
+def _get_pantanal_df(df):
+    """
+    Filter municipalities within Pantanal.
+    """
+
+    pantanal_df = df.copy()
+    dict_municipios_pantanal = {
+        "Baro de Melgao (MT)": "Barão de Melgaço (MT)",
+        "Cceres (MT)": "Cáceres (MT)",
+        "Itiquira (MT)": "Itiquira (MT)",
+        "Lambari D'Oeste (MT)": "Lambari D'Oeste (MT)",
+        "Nossa Senhora do Livramento (MT)": "Nossa Senhora do Livramento (MT)",
+        "Pocon (MT)": "Poconé (MT)",
+        "Santo Antnio do Leverger (MT)": "Santo Antônio do Leverger (MT)",
+        "Aquidauana (MS)": "Aquidauana (MS)",
+        "Bodoquena (MS)": "Bodoquena (MS)",
+        "Corumb (MS)": "Corumbá (MS)",
+        "Coxim (MS)": "Coxim (MS)",
+        "Ladrio (MS)": "Ladário (MS)",
+        "Miranda (MS)": "Miranda (MS)",
+        "Sonora (MS)": "Sonora (MS)",
+        "Porto Murtinho (MS)": "Porto Murtinho (MS)",
+        "Rio Verde de Mato Grosso (MS)": "Rio Verde de Mato Grosso (MS)",
+    }
+
+    pantanal_df["location"] = pantanal_df["location"].replace(dict_municipios_pantanal)
+
+    pantanal_df["municipality"] = (
+        pantanal_df["location"].str.split("(").str[0].str.strip()
+    )
+
+    pantanal_df = pantanal_df[
+        pantanal_df.location.isin(dict_municipios_pantanal.values())
+    ]
+
+    assert len(pantanal_df.location.unique()) == len(dict_municipios_pantanal)
+
+    return pantanal_df
 
 
 def run():
@@ -46,10 +89,12 @@ def run():
 
     # reading the data
     # df_land_use = io.load_table("raw", "land_use")
-    df_land_use_transitions = io.load_table("raw", "land_use_transitions")
+    df_land_use_transitions = io.load_table("raw", "land_use_transitions_new")
 
     # # pre-processing steps
-    data_p = _select_cols_and_treat_df(df_land_use_transitions)
+    data_p = (
+        df_land_use_transitions >> _select_cols_and_treat_df() >> _get_pantanal_df()
+    )
 
     return data_p
 
@@ -59,9 +104,6 @@ def save():
     Save the quantitys pre-processed data.
     """
     data_p = run()
-    import ipdb
-
-    ipdb.set_trace()
 
     io.save_table(data_p, "preprocessed", "land_use_transitions")
 
