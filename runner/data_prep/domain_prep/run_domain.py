de03e57b9f837fdb5c1a90e7553a5846e4f541cb
iglesias-caio-bcg <iglesias.caio@bcg.com>
2022-11-22 13:22:12 -0300

    update run_domain and run_raw_prep with queimadas and laws

--- runner/data_prep/domain_prep/run_domain.py
+++ runner/data_prep/domain_prep/run_domain.py
@@ -1,9 +1,13 @@
 from runner.data_prep.domain_prep import production_pantanal
 from runner.data_prep.domain_prep import deforestation_pantanal
 from runner.data_prep.domain_prep import area_pantanal_features
+from runner.data_prep.domain_prep import queimadas
+from runner.data_prep.domain_prep import environmental_laws
 
 
 def run_domain():
     production_pantanal.save()
     deforestation_pantanal.save()
     area_pantanal_features.save()
+    queimadas.save()
+    environmental_laws.save()
--- runner/data_prep/raw_prep/run_raw_prep.py
+++ runner/data_prep/raw_prep/run_raw_prep.py
@@ -3,6 +3,8 @@ from runner.data_prep.raw_prep import pecuaria
 from runner.data_prep.raw_prep import quantidade
 from runner.data_prep.raw_prep import land_use_transitions
 from runner.data_prep.raw_prep import land_use
+from runner.data_prep.raw_prep import environmental_laws
+from runner.data_prep.raw_prep import queimadas
 
 
 def run_raw_prep():
@@ -11,3 +13,5 @@ def run_raw_prep():
     quantidade.save()
     land_use_transitions.save()
     land_use.save()
+    environmental_laws.save()
+    queimadas.save()
